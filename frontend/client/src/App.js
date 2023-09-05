88023dda532daa360f06fda2e389a8e31b8fe4a6
lipin1 <lipinkariappa@gmail.com>
2023-09-05 13:16:21 +0530

    made the basic home page

--- frontend/client/src/App.js
+++ frontend/client/src/App.js
@@ -1,20 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
-import { H1, Button,Card } from './components/s1';
+import Main from './components/s1';
 
 function App() {
   return (
-    <>
-      <Button>
-        Chat now
-      </Button>
-      <Card>
-        
-        <Button>
-          Chat now
-        </Button>
-      </Card>
-    </>
+   <div>
+      <Main />
+   </div>
   );
 }
 
--- frontend/client/src/components/s1.jsx
+++ frontend/client/src/components/s1.jsx
@@ -2,45 +2,45 @@ import "../App.css"
 
 
 // Text Components
-export function H1({children,styles}){
+export function H1({children,addStyles}){
     return (
         <>
-        <h1 className="text-6xl" >{children}</h1>
+        <h1 className={"text-6xl "+ addStyles}  >{children}</h1>
         </>
     )
 }
-export function H2({children,styles}){
+export function H2({children,addStyles}){
     return (
         <>
-        <h1 className=" text-4xl">{children}</h1>
+        <h1 className={" text-4xl " + addStyles}>{children}</h1>
         </>
     )
 }
-export function H3({children,styles}){
+export function H3({children,addStyles}){
     return (
         <>
-        <h1 className=" text-2xl">{children}</h1>
+        <h1 className={" text-2xl " + addStyles}>{children}</h1>
         </>
     )
 }
-export function H4({children,styles}){
+export function H4({children,addStyles}){
     return (
         <>
-        <h1 className=" text-xl">{children}</h1>
+        <h1 className={" text-xl " +addStyles}>{children}</h1>
         </>
     )
 }
-export function H5({children,styles}){
+export function H5({children,addStyles}){
     return (
         <>
-        <h1 className=" text-md">{children}</h1>
+        <h1 className={" text-md " + addStyles}>{children}</h1>
         </>
     )
 }
-export function H6({children,styles}){
+export function H6({children,addStyles}){
     return (
         <>
-        <h1 className=" text-sm">{children}</h1>
+        <h1 className={" text-sm " + addStyles}>{children}</h1>
         </>
     )
 }
@@ -57,13 +57,48 @@ export function Button({children}){
 
 
 //Card component
-export function Card({children}){
+export function Card({children,addStyles}){
     return(
-        <div className="p-4 m-4 bg-slate-200 hover:shadow-sm shadow-slate-400  inline-block rounded-lg">
+        <div className={"p-4 m-4  bg-slate-200 hover:shadow-sm shadow-slate-400  inline-block rounded-lg " + addStyles}>
             {children}
         </div>
     )
 }
+
+
+// Input component
+export function Input({placeholder}){
+    return(
+        <input type="text"  placeholder={placeholder} className="bg-slate-300 p-4 focus:outline-none placeholder:text-slate-400 rounded-md tracking-widest font-semibold text-lg" />
+    )
+}
+
+export function Navbar(){
+    return(
+    <div className="absolute p-4  text-4xl text-white top-0 left-0 w-screen h-20 bg-transparent">
+        rANd0M
+    </div>)
+}
+
+ export default function App({children}){
+     return(
+        <div>
+            <Navbar />
+
+        <div className="w-screen h-screen flex flex-col items-center bg-slate-800">
+            <div className="text-center my-8 ">
+
+            <H1 addStyles={"text-white font-bold tracking-tighter"}>Join a random room</H1>
+            <H3 addStyles={"text-slate-300 tracking-wide font-light"}>yes,copied omegle,any probs?</H3>
+            </div>
+            <Card addStyles={'flex flex-col'}>
+                <Input placeholder={"random_username"} />
+                <Button>Join</Button>
+            </Card>
+        </div>
+        </div>
+     )
+ }
 
 
 
